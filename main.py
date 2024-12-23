4cd0f898deb65d5e1deb381e6fc30c37d21de5a6
AurelioGuilherme <109996864+AurelioGuilherme@users.noreply.github.com>
2024-12-23 03:54:00 -0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from utils import streamlit_utils, layout_indicium
-import dagshub
-dagshub.init(repo_owner='AurelioGuilherme', repo_name='AmbienteDeDesenvolvimento', mlflow=True)
+
 
 # Layout 
 layout_indicium.layout_custom()
